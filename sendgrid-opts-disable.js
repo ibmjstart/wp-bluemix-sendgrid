2051d374af9b8c5c91e9399448ac9e11ea1ff3b3
Austin Hamilton <karas_aj@outlook.com>
2014-12-09 10:46:25 -0500

    Remove disable api credentials

--- sendgrid-opts-disable.js
+++ sendgrid-opts-disable.js
@@ -2,7 +2,9 @@ jQuery(function() {
 	var form_ele = jQuery('form[name="sendgrid_form"]')
 	jQuery("<p><em>Credentials automatically retrieved from Bluemix.</em></p>").insertBefore(form_ele);
 	
-	jQuery('input[name=sendgrid_user]').attr('disabled', 'disabled');
-	jQuery('input[name=sendgrid_pwd]').attr('disabled', 'disabled');
-	jQuery('select[name=sendgrid_api]').attr('disabled', 'disabled');
+	// We can't disable these and still send the data when the form is submitted.
+	
+	// jQuery('input[name=sendgrid_user]').attr('disabled', 'disabled');
+	// jQuery('input[name=sendgrid_pwd]').attr('disabled', 'disabled');
+	// jQuery('select[name=sendgrid_api]').attr('disabled', 'disabled');
 });
